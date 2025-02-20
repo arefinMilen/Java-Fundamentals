f8817843c8012c5fb421f9e646a8312619b970cc
arefinMilen <arefin15-5279@diu.edu.bd>
2025-02-20 23:57:51 +0600

    added debug demo code

---
+++ Debug_Demo/Debug_Demo/Debug.java
@@ -0,0 +1,13 @@
+
+package Debug_Demo;
+
+
+public class Debug {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum+=i;
+            System.out.println(sum);
+        }
+    }
+}
